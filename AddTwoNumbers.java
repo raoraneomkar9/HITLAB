97f2d4a57422078fbb5efa2d5cd84128d6d173f7
raoraneomkar9 <84660202+raoraneomkar9@users.noreply.github.com>
2021-06-06 18:14:59 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1= 50,num2=150;
+		int sum;
+		sum=num1+num2;
+		System.out.println("sum of these number: "+sum);
+
+	}
+
+}
---
+++ QuotientAndRemainder.java
@@ -0,0 +1,13 @@
+
+public class QuotientAndRemainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1= 150,num2=20;
+		int quotient=num1/num2;
+		int remainder=num1%num2;
+		System.out.println("quotient of these number: "+quotient);
+		System.out.println("remainder of these number: "+remainder);
+	}
+
+}
