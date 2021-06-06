55b9ec6e6fc94ad0a79cbf5f836923b38fe8e67c
raoraneomkar9 <84660202+raoraneomkar9@users.noreply.github.com>
2021-06-06 18:33:02 +0530

    Add files via upload

---
+++ CheckEvenOdd.java
@@ -0,0 +1,15 @@
+
+public class CheckEvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=20;
+if(num%2==0) {
+	System.out.println("even");
+}
+else {
+	System.out.println("odd");
+}
+	}
+
+}
---
+++ Power.java
@@ -0,0 +1,15 @@
+
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int base=3, exponent=4;
+long result=1;
+while(exponent!=0) {
+	result *= base;
+	--exponent;
+}
+System.out.println("answer "+result);
+	}
+
+}
