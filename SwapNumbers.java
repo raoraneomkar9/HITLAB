6ce647f362d2675a5de2b49d3313a0d641fd9683
raoraneomkar9 <84660202+raoraneomkar9@users.noreply.github.com>
2021-06-06 18:33:41 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,20 @@
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1= 120,num2=220;
+		System.out.println("--before swap--");
+		System.out.println("first number "+num1);
+		System.out.println("second numer "+num2);
+		
+		
+		num1=num1-num2;
+		num2=num1+num2;
+		num1=num2-num1;
+		System.out.println("--after swap--");
+		System.out.println("first number: "+num1);
+		System.out.println("second number: "+num2);
+	}
+
+}
